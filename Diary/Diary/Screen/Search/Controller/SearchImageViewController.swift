403fd582d8a835e7c8ddd5869b0a321830d82311
heerucan <iwpow0307@gmail.com>
2022-08-21 19:33:31 +0900

    init: initial setting

---
+++ Diary/Diary/Screen/Search/Controller/SearchImageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchImageViewController.swift
+//  Diary
+//
+//  Created by heerucan on 2022/08/21.
+//
+
+import UIKit
+
+class SearchImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
