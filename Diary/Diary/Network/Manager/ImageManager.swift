8a0106f6aaf5cb9a3f627472acb03bb4d5ae7e60
heerucan <iwpow0307@gmail.com>
2022-08-22 23:05:55 +0900

    add: ImageManager API 구현

--- Diary/Diary/Network/Manager/ImageManager.swift
+++ Diary/Diary/Network/Manager/ImageManager.swift
@@ -13,8 +13,8 @@ import SwiftyJSON
 struct ImageManager {
     private init() { }
     static let shared = ImageManager()
-    typealias imageCompletion = ((JSON) -> Void)
-    typealias searchCompletion = ((JSON) -> Void)
+    typealias imageCompletion = (([URL]) -> Void)
+    typealias searchCompletion = (([URL], Int) -> Void)
     
     // MARK: - GET Unsplash Image
     
@@ -26,9 +26,8 @@ struct ImageManager {
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-                let imageList = json.arrayValue.map { $0["urls"]["full"].stringValue }
-                print(imageList)
-                
+                let imageList = json.arrayValue.map { URL(string: $0["urls"]["small"].stringValue)! }
+                imageCompletion(imageList)
                 
             case .failure(let error):
                 print(error)
@@ -38,15 +37,17 @@ struct ImageManager {
     
     // MARK: - GET Search Image by query
     
-    func requestSearch(page: Int, query: String, imageCompletion: @escaping imageCompletion) {
+    func requestSearch(page: Int, query: String, searchCompletion: @escaping searchCompletion) {
         
-        let url = URLType.makeURL(.image, page, query)
+        let url = URLType.makeURL(.search, page, query)
         
         AF.request(url, method: .get).validate(statusCode: 200..<400).responseData { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-                print(json)
+                let imageList = json["results"].arrayValue.map { URL(string: $0["urls"]["small"].stringValue)! }
+                let total = json["total"].intValue
+                searchCompletion(imageList, total)
                 
             case .failure(let error):
                 print(error)
