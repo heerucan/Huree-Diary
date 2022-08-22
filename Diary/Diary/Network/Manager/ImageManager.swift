43c5c6b6c083b2080a8a132b6bd93a9b4765ffcf
heerucan <iwpow0307@gmail.com>
2022-08-22 10:56:16 +0900

    add: ImageAPIManager 생성

--- Diary/Diary/Network/Manager/ImageManager.swift
+++ Diary/Diary/Network/Manager/ImageManager.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+import Alamofire
+import SwiftyJSON
+
+struct ImageManager {
+    private init() { }
+    static let shared = ImageManager()
+    typealias imageCompletion = ((JSON) -> Void)
+    typealias searchCompletion = ((JSON) -> Void)
+    
+    // MARK: - GET Unsplash Image
+    
+    func requestImage(page: Int, imageCompletion: @escaping imageCompletion) {
+        
+        let url = URLType.makeURL(.image, page)
+        
+        AF.request(url, method: .get).validate(statusCode: 200..<400).responseData { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let imageList = json.arrayValue.map { $0["urls"]["full"].stringValue }
+                print(imageList)
+                
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    // MARK: - GET Search Image by query
+    
+    func requestSearch(page: Int, query: String, imageCompletion: @escaping imageCompletion) {
+        
+        let url = URLType.makeURL(.image, page, query)
+        
+        AF.request(url, method: .get).validate(statusCode: 200..<400).responseData { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print(json)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
