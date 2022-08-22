d2eaf06e7974a98e585b32c4d1cce90bfcb639cd
heerucan <iwpow0307@gmail.com>
2022-08-22 22:50:10 +0900

    chore: 검색어로 이미지 찾는 URL로 변경

--- Diary/Diary/Network/Global/URLType.swift
+++ Diary/Diary/Network/Global/URLType.swift
@@ -15,17 +15,17 @@ enum URLType {
     var endpoint: String {
         switch self {
         case .search:
-            return URLType.baseURL + "/search/photos/"
+            return URLType.baseURL + "/search/photos/?per_page=50"
         case .image:
-            return URLType.baseURL + "/photos/"
+            return URLType.baseURL + "/photos/?per_page=50"
         }
     }
     
     static func makeURL(_ type: URLType, _ page: Int, _ query: String? = nil) -> String {
         if let query = query {
-            return type.endpoint + "?page=\(page)&query=\(query)&client_id=" + APIKey.accesskey
+            return type.endpoint + "&page=\(page)&query=\(query)&client_id=" + APIKey.accesskey
         } else {
-            return type.endpoint + "?page=\(page)&client_id=" + APIKey.accesskey
+            return type.endpoint + "&page=\(page)&client_id=" + APIKey.accesskey
         }
     }
 }
