0e78342369a7587f2ad8024c2c588a4766a6a115
heerucan <iwpow0307@gmail.com>
2022-08-22 20:54:53 +0900

    add: add Realm Schema - UserDiary

--- Diary/Realm/RealmModel.swift
+++ Diary/Realm/RealmModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+import RealmSwift
+
+// MARK: - UserDiary
+
+/*
+ UserDiary : 테이블이름
+ @Persisted : 컬럼명
+*/
+
+class UserDiary: Object {
+    @Persisted var title: String
+    @Persisted var content: String?
+    @Persisted var createdAt = Date() // 작성 날짜
+    @Persisted var updatedAt: String? // 일기의 진짜 날짜
+    @Persisted var favorite: Bool // 즐겨찾기
+    @Persisted var image: String?
+    
+    // PK, Primary Key : 중복되지 않은 Int, UUID or ObjectID
+    @Persisted(primaryKey: true) var objectId: ObjectId
+    
+    convenience init(title: String,
+                     content: String?,
+                     createdAt: Date,
+                     updatedAt: String?,
+                     image: String?) {
+        self.init()
+        self.title = title
+        self.content = content
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.favorite = false
+        self.image = image
+    }
+}
