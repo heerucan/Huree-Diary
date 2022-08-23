6f8c09c0bf80063a12227605eabdda6477a2b8de
heerucan <iwpow0307@gmail.com>
2022-08-24 03:53:13 +0900

    add: Error 프로토콜 따르는 Enum 추가

---
+++ Diary/Diary/Network/Global/ErrorType.swift
@@ -0,0 +1,14 @@
+//
+//  ErrorType.swift
+//  Diary
+//
+//  Created by heerucan on 2022/08/24.
+//
+
+import Foundation
+
+enum ErrorType: Error {
+    case invalid
+    case failure
+    case noData
+}
